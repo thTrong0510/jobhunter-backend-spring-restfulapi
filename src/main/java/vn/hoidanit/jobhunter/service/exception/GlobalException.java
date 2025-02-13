6541412d72e0ba71d57319ce6d276102f7cd2292
thTrong0510 <vthanhtrongng@gmail.com>
2025-02-13 20:50:45 +0700

    globla response -> format response before sending

--- src/main/java/vn/hoidanit/jobhunter/service/exception/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/service/exception/GlobalException.java
@@ -1,13 +1,22 @@
 package vn.hoidanit.jobhunter.service.exception;
 
+import java.net.http.HttpResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException) {
-        return ResponseEntity.badRequest().body(idInvalidException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idInvalidException) {
+        RestResponse<Object> restResponse = new RestResponse<Object>();
+        restResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        restResponse.setError(idInvalidException.getMessage());
+        restResponse.setMessage("error idInvalidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(restResponse);
     }
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
@@ -0,0 +1,48 @@
+package vn.hoidanit.jobhunter.util;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
+@ControllerAdvice
+@ResponseBody
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    @Nullable
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType,
+            Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        // TODO Auto-generated method stub
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestResponse<Object> restResponse = new RestResponse<Object>();
+        restResponse.setStatusCode(status);
+        if (status >= 400) {
+            // case error
+            return body;
+        } else {
+            // case success
+            restResponse.setData(body);
+            restResponse.setMessage("CALL API SUCCESS");
+        }
+
+        return restResponse;
+    }
+
+}
